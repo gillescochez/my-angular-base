699b1aad06141c03652d9224f5fa39a5f9aec841
Gilles Cochez <gillescochez@gmail.com>
2016-05-21 15:46:34 +0100

    Nav to login page and back

--- test/e2e/main.js
+++ test/e2e/main.js
@@ -19,4 +19,13 @@ describe("main", function() {
         expect(browser.getLocationAbsUrl()).toMatch("login");
     });
 
+    it("should redirect to the home page when the app name is clicked after going to the login page", function() {
+        element(by.css("#nav-login")).click();
+        browser.waitForAngular();
+        expect(browser.getLocationAbsUrl()).toMatch("login");
+        element(by.css(".navbar-brand")).click();
+        browser.waitForAngular();
+        expect(browser.getLocationAbsUrl()).toMatch("home");
+    });
+
 });
